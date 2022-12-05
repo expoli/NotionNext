d3726f8b46463260c90671c3f6cd6d98cb24d592
expoli <31023767+expoli@users.noreply.github.com>
2022-12-05 23:53:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: process.env.AUTHOR || 'tangcuyu', // 作者
   BIO: process.env.BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.LINK || 'https://expoli.tech', // 网站地址
+  LINK: process.env.LINK || 'https://www.expoli.tech', // 网站地址
   KEYWORDS: process.env.KEYWORDS || 'Notion, 博客, expoli, 糖醋鱼, tangcuyu', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
@@ -21,9 +21,9 @@ const BLOG = {
   THEME_SWITCH: process.env.NEXT_PUBLIC_THEME_SWITCH || false, // 是否显示切换主题按钮
   LANG: 'zh-CN', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   HOME_BANNER_IMAGE: './bg_image.jpg', // 首页背景大图，默认文件：/public/bg_image.jpg 。会被Notion中的封面图覆盖。
-  SINCE: 2021, // e.g if leave this empty, current year will be used.
+  SINCE: process.env.SINCE || '2021', // e.g if leave this empty, current year will be used.
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXXX
-  APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
+  APPEARANCE: process.env.APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
   // 自定义字体示例： 请先将 CUSTOM_FONT 改为 true， 并将 CUSTOM_FONT_URL 改为你的字体CSS地址，同时在 CUSTOM_FONT_SANS 与 CUSTOM_FONT_SERIF 中指定你的 fontfamily
   CUSTOM_FONT: true, // 是否使用自定义字体
@@ -52,16 +52,16 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: process.env.CONTACT_EMAIL || 'mail@tangly1024.com', // 邮箱
+  CONTACT_EMAIL: process.env.CONTACT_EMAIL, // 邮箱
   CONTACT_WEIBO: process.env.CONTACT_WEIBO, // 你的微博个人主页
   CONTACT_TWITTER: process.env.CONTACT_TWITTER, // 你的twitter个人主页
-  CONTACT_GITHUB: process.env.CONTACT_GITHUB || 'https://github.com/tangly1024', // 你的github个人主页
+  CONTACT_GITHUB: process.env.CONTACT_GITHUB || 'https://github.com/expoli', // 你的github个人主页
   CONTACT_TELEGRAM: process.env.CONTACT_TELEGRAM, // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.CONTACT_LINKEDIN, // 你的linkedIn 首页
 
 
   // 鼠标点击烟花特效
-  FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || false, // 鼠标点击烟花特效
+  FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 鼠标点击烟花特效
 
   // 悬浮挂件
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
